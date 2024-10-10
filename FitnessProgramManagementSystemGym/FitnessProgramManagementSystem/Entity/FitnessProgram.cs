c85f3120ed54460b1cfd0f99bcccf67c485c054b
Anne tharshana <UT01631@unicomtic.com>
2024-10-10 12:04:04 +0530

    Question3

--- FitnessProgramManagementSystemGym/FitnessProgramManagementSystem/Entity/FitnessProgram.cs
+++ FitnessProgramManagementSystemGym/FitnessProgramManagementSystem/Entity/FitnessProgram.cs
@@ -8,14 +8,24 @@ namespace FitnessProgramManagementSystem.Entity
 {
     internal class FitnessProgram
     {
+        public FitnessProgram(int fitnessProgramId, string title, string duration, decimal price)
+        {
+            FitnessProgramId = fitnessProgramId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
 
         public int FitnessProgramId { get; set; }
         public string Title { get; set; }
         public string Duration { get; set; }
         public decimal Price { get; set; }
 
-       
 
+        public override string ToString() {
+            return $"ID: {FitnessProgramId}, Title: {Title}, Duration: {Duration}, Price: {Price}";
+                
+                }
 
 
 

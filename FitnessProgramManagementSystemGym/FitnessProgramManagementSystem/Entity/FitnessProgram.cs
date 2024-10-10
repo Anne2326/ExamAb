c92ad98a8c488cff71e1cd5cc05d8393868a553d
Anne tharshana <UT01631@unicomtic.com>
2024-10-10 12:01:00 +0530

    My first Project

---
+++ FitnessProgramManagementSystemGym/FitnessProgramManagementSystem/Entity/FitnessProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem.Entity
+{
+    internal class FitnessProgram
+    {
+
+        public int FitnessProgramId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+       
+
+
+
+
+
+
+
+    }
+
+}

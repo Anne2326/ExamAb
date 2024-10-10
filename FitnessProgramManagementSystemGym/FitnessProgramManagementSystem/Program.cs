73f8b6b68441ff86832c7befd2cb172ae6755f8e
Anne tharshana <UT01631@unicomtic.com>
2024-10-10 13:13:24 +0530

    Question5

--- FitnessProgramManagementSystemGym/FitnessProgramManagementSystem/Program.cs
+++ FitnessProgramManagementSystemGym/FitnessProgramManagementSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FitnessProgramManagementSystem.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,94 @@ namespace FitnessProgramManagementSystem
     {
         static void Main(string[] args)
         {
+            Menu();
+            Console.ReadLine();
+
         }
+
+        public static void Menu()
+        {
+
+
+            var admin = new FitnessProgramManager();
+
+
+            Console.WriteLine("FitnessProgram Management System:");
+
+            Console.WriteLine("1. Add a FitnessProgram");
+
+            Console.WriteLine("2. View All FitnessPrograms");
+
+            Console.WriteLine("3. Update a FitnessProgram");
+
+
+            Console.WriteLine("4. Delete a FitnessProgram");
+
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option:");
+
+
+
+
+
+
+
+            var MenuInput=Console.ReadLine();
+            switch (MenuInput) {
+
+                case "1":
+                    admin.CreateFitnessProgram();
+                    break;
+                    case "2":
+                    admin.ReadFitnessPrograms();
+                    break;
+                    case "3":
+                    admin.UpdateFitnessProgram();
+                    break;
+                    case "4":
+                    admin.DeleteFitnessProgram();
+                    break;
+                    case "5":
+                    Console.WriteLine("Thank you");
+                    break;
+                    default:
+                    Console.Clear();
+                    Menu();
+                    break;
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
